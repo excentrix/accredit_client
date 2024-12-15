cf801498aae751be1d71ba02cf0cc433da8f9400
SidKarthik <sa268592@gmail.com>
2024-12-15 20:33:43 +0530

    bug fix

--- app/(auth)/reset-password/request/page.tsx
+++ app/(auth)/reset-password/request/page.tsx
@@ -71,8 +71,8 @@ export default function RequestResetPage() {
           </Button>
           <CardTitle>Reset Password</CardTitle>
           <CardDescription>
-            Enter your email address and we'll send you a link to reset your
-            password.
+            Enter your email address and we&apos;ll send you a link to reset
+            your password.
           </CardDescription>
         </CardHeader>
         <CardContent>
